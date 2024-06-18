060b1ba017c2c8d33971741236a128beaaec1c4c
Adrian Gottschling <adrian.gottschling@gmail.com>
2024-06-18 21:55:42 +0200

    Edit route: /status Issue: #1
    - receives data from mc server now
    - add some util functions like parse_list_response
    
    Signed-off-by: Adrian Gottschling <adrian.gottschling@gmail.com>

--- app/routes.py
+++ app/routes.py
@@ -1,17 +1,30 @@
 from flask import Blueprint, request, jsonify
 from flask_jwt_extended import create_access_token, jwt_required
 from .rcon import send_rcon_command
+from .utils import parse_list_response
 
 bp = Blueprint('main', __name__)
 
+
 @bp.route('/status', methods=['GET'])
 def server_status():
-    status = {
-        "online": True,    # TODO: Implement logic to check server status
-        "players": 5,
-        "maxPlayers": 10
-    }
-    return jsonify(status)
+    try:
+        response_players = send_rcon_command('list')
+        players_info = parse_list_response(response_players)
+
+        status = {
+            "online": True,     # Assuming server is online if RCON commands succeed
+            "current_players": players_info['current_players'],
+            "max_players": players_info['max_players'],
+            "players": players_info['players_list']
+        }
+
+        return jsonify(status)
+
+    except Exception as e:
+        # Handle exceptions (e.g., RCON connection failure)
+        error_message = {"error": str(e)}
+        return jsonify(error_message), 500
 
 
 @bp.route('/rcon/command', methods=['POST'])
